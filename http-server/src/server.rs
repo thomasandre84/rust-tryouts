2d0db0ff4f81f0325cda6647439ae7b5ecefb9fa
Thomas Andre <thomasandre84@gmail.com>
2023-02-27 20:11:52 +0100

    continue

--- http-server/src/server.rs
+++ http-server/src/server.rs
@@ -1,3 +1,9 @@
+use std::net::TcpListener;
+use std::io::Read;
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
+
 pub struct Server {
     addr: String
 }
@@ -10,6 +16,34 @@ impl Server{
     }
 
     pub fn run(self) {
-        println!("Listening on {}", self.addr)
+        println!("Listening on {}", self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer){
+                        Ok(_) => {
+                            println!("Recieved a request: {}", String::from_utf8_lossy(&buffer));
+
+                            //let res: &Result<Request, _> = &buffer[..].try_into();
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {},
+                                Err(e) => println!("Failed to parse request: {}", e)
+                            }
+                            
+                        },
+                        Err(e) => println!("Failed to read from Connection: {}", e)
+                    }
+
+
+                },
+                Err(e) => println!("Failed to establish a connection: {}", e)
+            }
+        }
+
     }
 }
